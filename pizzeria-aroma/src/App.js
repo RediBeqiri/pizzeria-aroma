7518c03afc72909bcda07786dd215f4a1590e70f
RediBeqiri <rbeqiri08@gmail.com>
2025-04-29 12:18:42 +0200

    navbar

--- pizzeria-aroma/src/App.js
+++ pizzeria-aroma/src/App.js
@@ -1,47 +1,28 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Hero from "./components/Hero";
 import Menu from "./components/Menu";
-import Reviews from "./components/Reviews"; // Assuming you have a Reviews component
+import Reviews from "./components/Reviews";
 import BestSellers from "./components/BestSellers";
+import Navbar from "./components/Navbar"; // <- already imported
 import "./App.css";
 
 const App = () => {
   return (
     <Router>
       <div className="App">
-        {/* Header with Menu and other links */}
-        <header>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">
-                  <button className="home-btn">Home</button> {/* Link to the Home page */}
-                </Link>
-              </li>
-              <li>
-                <Link to="/menu">
-                  <button className="menu-btn">Our Menu</button> {/* Link to Menu page */}
-                </Link>
-              </li>
-              <li>
-                <Link to="/reviews">
-                  <button className="reviews-btn">Reviews</button> {/* Link to Reviews page */}
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </header>
+        {/* Modern Navbar */}
+        <Navbar />
 
         {/* Set up the routes */}
         <Routes>
-          {/* Home Page Route: This will show the Hero and Best Sellers sections */}
+          {/* Home Page Route: Hero + Best Sellers */}
           <Route
             path="/"
             element={
               <div>
                 <Hero />
-                <BestSellers /> {/* Best Sellers section on the home page */}
+                <BestSellers />
               </div>
             }
           />
